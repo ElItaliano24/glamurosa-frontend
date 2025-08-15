f5e1f67aa09bd955455b1751c3314813afab0453
Fabricio Miranda <mirandaquezadafabriciojoel@gmail.com>
2025-08-15 01:32:09 -0500

    desarrollo de las card y consumo de API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,73 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useState } from "react";
+import Image from 'next/image';
+
+
+interface Media {
+  id: string;
+  url?: string;
+  filename?: string;
+  mimeType?: string;
+  // añade otros campos si los necesitas
+}
+
+interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  images?: {
+    image?: Media;
+  }[];
+}
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [products, setProducts] = useState<Product[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const res = await fetch("http://localhost:3001/api/products");
+        const data = await res.json();
+        setProducts(data.docs);
+        console.log(data.docs)
+
+      } catch (error) {
+        console.error("Failed to fetch products:", error);
+      }
+    }
+    fetchProducts();
+  }, []);
+
+  return (
+    <main className="bg-[#E1DFDB]">
+      <h1 className="font-bold text-3xl text-center py-2">PRODUCTOS</h1>
+      <ul className="grid grid-cols-1 items-center justify-center gap-5 mx-2">
+        {products.map((p) => (
+          <li key={p.id} className="flex gap-5 items-start justify-center">
+            <div className="border w-[150px] aspect-[3/4] relative overflow-hidden rounded-sm">
+            {p.images?.[0]?.image?.url && (
+              <Image
+              src={p.images[0].image.url}
+              alt={p.name}
+              fill
+              priority={true}
+              className="object-cover"
+              />
+            )}
+            </div>
+            <div className="flex-1">
+            <h2 className="font-bold text-md mb-2">{p.name}</h2>
+            <p className="text-sm mb-2">{p.description}</p>
+            <p className="font-bold text-lg">S/ {p.price}</p>
+            </div>
+
+
+          </li>
+        ))}
+      </ul>
+
+    </main>
   );
 }
