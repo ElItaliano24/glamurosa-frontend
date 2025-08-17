a655d562d0fd42e380be3c83b1b1bcf891337518
Fabricio Miranda <mirandaquezadafabriciojoel@gmail.com>
2025-08-17 00:55:14 -0500

    modificacion de url de api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
   useEffect(() => {
     async function fetchProducts() {
       try {
-        const res = await fetch("http://localhost:3001/api/products");
+        const res = await fetch("https://glamurosa-backend.vercel.app/api/products");
         const data = await res.json();
         setProducts(data.docs);
         console.log(data.docs)
@@ -43,27 +43,31 @@ export default function Home() {
   return (
     <main className="bg-[#E1DFDB]">
       <h1 className="font-bold text-3xl text-center py-2">PRODUCTOS</h1>
-      <ul className="grid grid-cols-1 items-center justify-center gap-5 mx-2">
+      <ul className="grid grid-cols-1 items-center justify-center gap-5 mx-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {products.map((p) => (
           <li key={p.id} className="flex gap-5 items-start justify-center">
             <div className="border w-[150px] aspect-[3/4] relative overflow-hidden rounded-sm">
-            {p.images?.[0]?.image?.url && (
-              <Image
-              src={p.images[0].image.url}
-              alt={p.name}
-              fill
-              priority={true}
-              className="object-cover"
-              />
-            )}
+              {p.images?.[0]?.image?.url && (
+                <Image
+                  src={p.images[0].image.url}
+                  alt={p.name}
+                  fill
+                  priority={true}
+                  className="object-cover"
+                />
+              )}
             </div>
             <div className="flex-1">
-            <h2 className="font-bold text-md mb-2">{p.name}</h2>
-            <p className="text-sm mb-2">{p.description}</p>
-            <p className="font-bold text-lg">S/ {p.price}</p>
+              <h2 className="font-bold text-md mb-2">{p.name}</h2>
+              <p className="text-sm mb-2">{p.description}</p>
+              <p className="font-bold text-lg mb-2">S/ {p.price}</p>
+              <a href={`https://wa.me/51980947986?text=${encodeURIComponent(
+                `Hola, estoy interesado en el producto "${p.name}" con precio S/ ${p.price}`
+              )}`} target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-2 text-xs bg-[#189D0E] text-white font-semibold py-2 px-4 rounded">
+                <Image src="/logo-whatsapp.svg" alt="logo de WhatsApp" width={24} height={24} />
+                Consultar
+              </a>
             </div>
-
-
           </li>
         ))}
       </ul>
