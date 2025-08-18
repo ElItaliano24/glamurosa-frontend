332033a2a5330bd4b029cd83f8d3ac3f5af011ff
Italiano <mirandaquezadafabriciojoel@gmail.com>
2025-08-18 03:11:55 -0500

    modificaicones

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,16 @@ const nextConfig: NextConfig = {
         port: "3001",
         pathname: "/api/media/file/**",
       },
+      {
+        protocol: 'https',
+        hostname: 'glamurosa-backend.vercel.app',
+        pathname: '/api/media/file/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/dcifqfvi7/**',
+      },
     ],
   },
 };
